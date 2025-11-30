81a04186dec6bcf2d538fe2ec60d5a14fc243597
adriancerralbo <135849253+adriancerralbo@users.noreply.github.com>
2025-11-30 21:50:04 +0100

    Update Main.java

--- src/electoralFormulas/Main.java
+++ src/electoralFormulas/Main.java
@@ -1,77 +1,74 @@
 package electoralFormulas;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class Main {
+
 	public static void main(String[] args) throws Exception {
+		Proportional proportional = new Proportional();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		JFrame frame = new JFrame("Electoral formulas");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(screenSize.width / 2, screenSize.height / 2);
+		frame.setLocation(screenSize.width / 4, screenSize.height / 4);
 
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<String> list = new ArrayList<>();
-		ArrayList<Integer> votes = new ArrayList<>();
-		boolean follow = true;
-		String line = null;
-		String[] data = null;
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panel.setLayout(new GridLayout(13, 1));
+		JLabel labelName = new JLabel("Name of the participant");
+		panel.add(labelName);
+		JTextField name = new JTextField(20);
+		panel.add(name);
+		JLabel labelVote = new JLabel("Vote of the participant");
+		panel.add(labelVote);
+		JTextField vote = new JTextField(20);
+		panel.add(vote);
+		JLabel information = new JLabel();
+		panel.add(information);
+		JButton button = new JButton("Add register");
+		panel.add(button);
+		button.addActionListener(e -> proportional.addRegisterButton(information, name.getText(), vote.getText()));
+		JButton buttonFile = new JButton("Import CSV");
+		buttonFile.addActionListener(e -> proportional.importCsv(frame, information));
+		panel.add(buttonFile);
 
-		System.out.println("##################################");
-		System.out.println("THIS IS A PROGRAM TO COMPUTE SEATS");
-		System.out.println("##################################");
-		System.out.println();
-		System.out.println("Type the participants and their votes in the following format:");
-		System.out.println("NAME VOTES");
-		System.out.println("Intro an empty line to finish.");
-		System.out.println();
+		frame.add(panel);
+		JLabel labelSeats = new JLabel("Total number of seats");
+		panel.add(labelSeats);
+		JTextField totalSeats = new JTextField(20);
+		panel.add(totalSeats);
 
-		while (follow) {
-			line = scanner.nextLine();
-			if (line.isEmpty()) {
-				follow = false;
-			} else {
-				try {
-					data = line.split(" ");
-					list.add(data[0]);
-					votes.add(Integer.parseInt(data[1]));
-				} catch (Exception e) {
-					System.err.println("This data is not valid.");
-				}
-			}
-		}
-
-		System.out.println("Type the number of total seats:");
-		follow = true;
-		int n = 0;
-
-		while (follow) {
-			try {
-				n = scanner.nextInt();
-				follow = false;
-			} catch (Exception e) {
-				System.err.println("This data is not valid");
-			}
-		}
-
-		System.out.println("Select the formula:");
 		Object[] formulas = EnumFormulas.class.getEnumConstants();
-		follow = true;
-		int k = 0;
+		String[] options = new String[formulas.length];
 
 		for (int i = 0; i < formulas.length; i++) {
-			System.out.println(Integer.toString(i + 1) + " " + formulas[i]);
+			options[i] = formulas[i].toString();
 		}
 
-		while (follow) {
-			try {
-				k = scanner.nextInt();
-				if (1 < k || formulas.length < k) {
-					throw new Exception();
-				}
-				follow = false;
-			} catch (Exception e) {
-				System.err.println("This data is not valid");
-			}
-		}
+		JComboBox<String> comboBox = new JComboBox<>(options);
+		panel.add(comboBox);
+		JButton buttonCompute = new JButton("Compute");
+		JLabel result = new JLabel();
+		buttonCompute.addActionListener(e -> proportional.buttonCompute(comboBox, result, totalSeats.getText()));
+		JScrollPane scrollPane = new JScrollPane(result);
+		panel.add(scrollPane);
+		panel.add(buttonCompute);
+		JButton reset = new JButton("Reset");
+		panel.add(reset);
+		reset.addActionListener(e -> proportional.resetLists(information, result));
 
-		Proportional prop = new Proportional(n, list, votes, EnumFormulas.valueOf(formulas[k - 1].toString()));
-		System.out.println(prop.toString());
+		frame.setVisible(true);
 	}
 }
